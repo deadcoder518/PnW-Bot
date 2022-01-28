a3df6c96f74c8ce85e538df0c4a987b49c4a1505
Hamzah Mohamed <hamzahmo2002@gmail.com>
2022-01-28 22:34:58 +0000

    First commit

---
+++ bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv.main import load_dotenv
+from discord.ext import commands
+import requests
+
+load_dotenv()
+
+bot = commands.Bot(command_prefix="!")
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+@bot.command(name="whoxen")
+async def identify(ctx):
+    xen = "Xen is the greatest of all time!"
+    await ctx.send(xen)
+    #could've done embeds but they're very easy!
+
+tradeData = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource=steel&key="+os.getenv("API_KEY"))
+tradeData = tradeData.json()
+@bot.command(name="steelsell")
+async def steelsell(ctx):
+    nationid = tradeData["lowestbuy"]["nationid"]
+    nationlink = "https://politicsandwar.com/nation/id="+nationid
+    await ctx.send(nationlink)
+
+bot.run(TOKEN)

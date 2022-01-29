a4511d1360a33095e9c921b8213f053c6118428d
Hamzah Mohamed <hamzahmo2002@gmail.com>
2022-01-29 14:44:26 +0000

    changed

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def steelsell(ctx):
     nationlink = "https://politicsandwar.com/nation/id="+nationid
     embed = discord.Embed(url=nationlink)
     embed.add_field(name="Time of Trade",value=date)
-    await ctx.send(embed)
+    await ctx.send(embed=embed)
 
 #running bot
 bot.run(TOKEN)

e6a55e2544fb84da9b0836a17e94e807ce734c3a
Hamzah Mohamed <hamzahmo2002@gmail.com>
2022-01-29 00:52:14 +0000

    changed

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import os
 from dotenv.main import load_dotenv
 from discord.ext import commands
 import requests
-import json
 
 load_dotenv()
 
@@ -16,16 +15,14 @@ async def identify(ctx):
     await ctx.send(xen)
     #could've done embeds but they're very easy!
 
-f = open("config.json")
-data = json.load(f)
-key = data["key"]
+key = os.getenv("API_KEY")
+
+tradeData = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource=steel&key="+key)
+tradeData = tradeData.json()
 
 #fetches buy steel price
 @bot.command(name="steelsell")
 async def steelsell(ctx):
-    tradeData = requests.get(f"https://politicsandwar.com/api/tradeprice/?resource=steel&key="+key)
-    tradeData = tradeData.json()
-    print(tradeData)
     nationid = tradeData["lowestbuy"]["nationid"]
     nationlink = "https://politicsandwar.com/nation/id="+nationid
     await ctx.send(nationlink)
